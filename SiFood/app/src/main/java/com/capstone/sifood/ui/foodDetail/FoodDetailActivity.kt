842058e9cb2e3fe76c034a61011916d109a47d07
1808561072_IP Rama <71679235+IPRamaAnadya@users.noreply.github.com>
2021-12-01 22:37:23 +0800

    menambahkan dependency hdodenhof.circleimageview, menambahkan halaman detail makanan

---
+++ SiFood/app/src/main/java/com/capstone/sifood/ui/foodDetail/FoodDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.capstone.sifood.ui.foodDetail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.sifood.R
+
+class FoodDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_food_detail)
+    }
+}
