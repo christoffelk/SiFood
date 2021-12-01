a62470dcec5da19387f8307181a4d1c28406fff8
christoffelk <65440283+christoffelk@users.noreply.github.com>
2021-12-01 17:55:24 +0700

    Menambahkan design pada layout fragment
    membuat design untuk layout fragment home, fragment article dan juga fragment favorite
    membuat file baru yaitu itemlist untuk recyclerview pada fragment home dan favorite
    membuat file baru yaitu article list untuk recyclerview pada fragment article

---
+++ src/main/java/com/capstone/sifood/ui/article/ArticleFragment.kt
@@ -0,0 +1,40 @@
+package com.capstone.sifood.ui.article
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.capstone.sifood.databinding.FragmentArticleBinding
+
+class ArticleFragment : Fragment() {
+
+    private lateinit var articleViewModel: ArticleViewModel
+    private var _binding: FragmentArticleBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        articleViewModel =
+            ViewModelProvider(this)[ArticleViewModel::class.java]
+
+        _binding = FragmentArticleBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
