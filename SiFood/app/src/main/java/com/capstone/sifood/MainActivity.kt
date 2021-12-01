01767a69d8aadbab856cb4c34e72a7cd9d06c1c5
1808561072_IP Rama <71679235+IPRamaAnadya@users.noreply.github.com>
2021-12-01 22:46:54 +0800

    menambahkan fungsi untuk mendapatkan lokasi user saat aplikasi dibuka

--- SiFood/app/src/main/java/com/capstone/sifood/MainActivity.kt
+++ SiFood/app/src/main/java/com/capstone/sifood/MainActivity.kt
@@ -1,23 +1,42 @@
 package com.capstone.sifood
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.capstone.sifood.databinding.ActivityMainBinding
+import com.capstone.sifood.other.Constant
+import com.capstone.sifood.other.Constant.PERMISSION_ID
+import com.google.android.gms.location.FusedLocationProviderClient
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        getLastLocation()
 
         val navView: BottomNavigationView = binding.navView
 
@@ -31,5 +50,66 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    fun CheckPermission():Boolean {
+        if(
+            ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED ||
+            ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ){
+            return true
+        }
+
+        return false
+
+    }
+
+    fun RequestPermission(){
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION,android.Manifest.permission.ACCESS_FINE_LOCATION),
+            PERMISSION_ID
+        )
+    }
+
+    @SuppressLint("MissingPermission")
+    fun getLastLocation(){
+        if(CheckPermission()){
+            if(isLocationEnabled()){
+                fusedLocationProviderClient.lastLocation.addOnCompleteListener {task->
+                    var location: Location? = task.result
+                    if(location == null){
+                        Toast.makeText(this,"Please Turn on Your device Location", Toast.LENGTH_SHORT).show()
+                    }else{
+                        Constant.LONGITUDE = location.longitude.toString()
+                        Constant.LATITUDE = location.latitude.toString()
+                        getLocationName(location.latitude,location.longitude)
+                    }
+                }
+            }else{
+                Toast.makeText(this,"Please Turn on Your device Location", Toast.LENGTH_SHORT).show()
+            }
+        }else{
+            RequestPermission()
+        }
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        var locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER)
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun getLocationName(lat: Double, long: Double){
+        val geoCoder = Geocoder(this, Locale.getDefault())
+        val Adress = geoCoder.getFromLocation(lat,long,3)
+        Log.d("Debug:",Adress.get(0).toString())
     }
 }
--- SiFood/app/src/main/java/com/capstone/sifood/other/Constant.kt
+++ SiFood/app/src/main/java/com/capstone/sifood/other/Constant.kt
@@ -2,4 +2,8 @@ package com.capstone.sifood.other
 
 object Constant {
     const val FOOD_COLLECTION = "makanan"
+    var LONGITUDE = ""
+    var LATITUDE = ""
+    val PERMISSION_ID = 1010
+
 }
