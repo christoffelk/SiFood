b37d96d5cd0e59d9f3d7887c41ffc4ca885754c0
1808561072_IP Rama <71679235+IPRamaAnadya@users.noreply.github.com>
2021-12-08 13:32:32 +0800

    Revert "Revert "Merge remote-tracking branch 'origin/main'""
    This reverts commit 7a771a3a6ed35d7b58e1bfd4513c1aa3b9ea1d4e.

--- SiFood/app/src/main/java/com/capstone/sifood/MainActivity.kt
+++ SiFood/app/src/main/java/com/capstone/sifood/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.capstone.sifood
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import androidx.preference.PreferenceManager
 import com.capstone.sifood.databinding.ActivityMainBinding
 import com.capstone.sifood.other.Constant.LATITUDE
 import com.capstone.sifood.other.Constant.LOCATION_NAME
 import com.capstone.sifood.other.Constant.LONGITUDE
 import com.capstone.sifood.other.LocationPicker
+import com.capstone.sifood.ui.setting.SettingActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.FirebaseApp
 
@@ -48,5 +54,34 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+    override fun onStart() {
+        super.onStart()
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        when(sharedPreferences.getString("key_dark_mode",""))
+        {
+            "auto" -> updateTheme(0)
+            "off" -> updateTheme(1)
+            "on" -> updateTheme(2)
+        }
+    }
+    private fun updateTheme(mode: Int): Boolean {
+        AppCompatDelegate.setDefaultNightMode(mode)
+        return true
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId)
+        {
+            R.id.action_settings -> {
+                val intent = Intent(this,SettingActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        return true
     }
 }
--- SiFood/app/src/main/java/com/capstone/sifood/ui/articledetail/ArticleDetailActivity.kt
+++ SiFood/app/src/main/java/com/capstone/sifood/ui/articledetail/ArticleDetailActivity.kt
@@ -2,16 +2,30 @@ package com.capstone.sifood.ui.articledetail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.webkit.WebView
+import androidx.lifecycle.ViewModelProvider
 import com.capstone.sifood.R
+import com.capstone.sifood.databinding.ActivityArticleDetailBinding
 
 class ArticleDetailActivity : AppCompatActivity() {
+    private lateinit var _binding : ActivityArticleDetailBinding
+    private val binding get() = _binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_article_detail)
+        _binding = ActivityArticleDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val articleDetailViewModel = ViewModelProvider(this)[ArticleDetailViewModel::class.java]
         val webView =findViewById<WebView>(R.id.detail_article)
         val link = intent.getStringExtra(URL) as String
-        webView.loadUrl(link)
+        binding.loading.visibility = View.VISIBLE
+        articleDetailViewModel.getUrl(link)
+        articleDetailViewModel.text.observe(this,{
+            webView.loadUrl(it)
+            binding.loading.visibility = View.GONE
+        })
+
+
     }
     companion object{
         const val URL ="url"
