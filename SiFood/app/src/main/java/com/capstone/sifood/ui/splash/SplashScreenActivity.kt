c1181221d2955bb3d2e2d0054fdd727d7291ef5f
1808561072_IP Rama <71679235+IPRamaAnadya@users.noreply.github.com>
2021-12-23 21:28:43 +0800

    mengganti icon

--- SiFood/app/src/main/java/com/capstone/sifood/ui/splash/SplashScreenActivity.kt
+++ SiFood/app/src/main/java/com/capstone/sifood/ui/splash/SplashScreenActivity.kt
@@ -8,17 +8,23 @@ import android.os.Handler
 import android.os.Looper
 import com.capstone.sifood.ui.main.MainActivity
 import com.capstone.sifood.R
+import com.capstone.sifood.databinding.ActivitySplashScreenBinding
 import com.capstone.sifood.ui.login.LoginActivity
 import com.google.firebase.auth.FirebaseAuth
 
 @SuppressLint("CustomSplashScreen")
 class SplashScreenActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivitySplashScreenBinding
+
     private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         auth = FirebaseAuth.getInstance()
 
         val currentUser = auth.currentUser
